857947cfd1da7ec116f17a39210d47310cbf4b69
R.E. Stern <robertstern@students.berkeley.net>
2018-01-18 23:04:15 -0800

    message

--- qb-reader.js
+++ qb-reader.js
@@ -5,15 +5,41 @@ var question = "This countryʹs city of Valledupar [VYE-ay-doo-PAR] is the origi
     "American country in which the Marxist group FARC [fark] operated.";
 
 $(document).ready(function(){
-    console.log(process(question));
+    console.log(process(cut(question)));
 });
 
-function process(string){
+function cut(string){
     var theArr = [];
     var start = 0;
     for(var i=0; i<string.length; i++){
         if(string.substring(i,i+1)==" "){
-            //cut into words
+            theArr.push(string.substring(start,i));
+            start=i+1;
         }
     }
+    theArr.push(string.substring(start,string.length));
+    return theArr;
+}
+
+function process(array){
+    var theNewArray = [];
+    var bonus = true;
+    var starttag;
+    for(var i=0; i<array.length; i++){
+        starttag="<span ";
+        if(bonus){
+            starttag += "class='power";
+        }else{
+            starttag += "class='outta-power";
+        }
+        if(array[i].substr(0,1)=="[" || array[i].substr(array[i].length-1,1)=="]"){
+            starttag+= " bracket";
+        }
+        starttag+="'>";
+        // if(/*current is a (*) */){
+        //
+        // }
+        theNewArray.push(starttag+array[i]+"</span>");
+    }
+    return theNewArray;
 }
